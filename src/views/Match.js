c9a1640e801cd3a349e7f5975d5989facc524614
Atharva J <caplock221b@gmail.com>
2021-12-10 22:36:19 +0530

    added skeleton to Match page

--- src/views/Match.js
+++ src/views/Match.js
@@ -6,6 +6,7 @@ import { axiosInstance } from "../util/axiosConfig";
 import { AiFillCaretRight, AiOutlinePause } from "react-icons/ai";
 import WebPlayer from "../components/WebPlayer";
 import { setMediaSession } from "../util/functions";
+import Skeleton from "../components/Skeleton";
 
 const Match = () => {
   const { matchHandle } = useParams();
@@ -70,137 +71,192 @@ const Match = () => {
       <div className="first-page">
         <div className="topbar">
           <div className="image-container">
-            <img
-              src={(store.me && store.me.image_url) || logo}
-              alt={store.me && store.me.name}
-            />
-            <img
-              src={(store.friend && store.friend.image_url) || logo}
-              alt={store.friend && store.friend.name}
-            />
+            {!store.me ? (
+              <>
+                <Skeleton type="circle" />
+                <Skeleton type="circle" />
+              </>
+            ) : (
+              <>
+                <img
+                  src={(store.me && store.me.image_url) || logo}
+                  alt={store.me && store.me.name}
+                />
+                <img
+                  src={(store.friend && store.friend.image_url) || logo}
+                  alt={store.friend && store.friend.name}
+                />
+              </>
+            )}
           </div>
           <p className="name-container">
-            {`${store.me && store.me.name.split(" ")[0]}`} x{" "}
-            {`${store.friend && store.friend.name.split(" ")[0]}`}
+            {!store.me && !store.friend ? (
+              <Skeleton type="text" />
+            ) : (
+              <>
+                {`${store.me && store.me.name.split(" ")[0]}`} x{" "}
+                {`${store.friend && store.friend.name.split(" ")[0]}`}
+              </>
+            )}
           </p>
         </div>
         <div className="middle-content">
-          <div className="text">
-            <p>
-              That’s a harmony we have never heard of before! Send a song to
-              give a spark to this vibe.
-            </p>
-          </div>
-          <div className="button-container">
-            <button>Send a Song</button>
-          </div>
+          {!store.me ? (
+            <>
+              <Skeleton type="text" />
+              <Skeleton type="text" />
+            </>
+          ) : (
+            <>
+              <div className="text">
+                <p>
+                  That’s a harmony we have never heard of before! Send a song to
+                  give a spark to this vibe.
+                </p>
+              </div>
+              <div className="button-container">
+                <button>Send a Song</button>
+              </div>
+            </>
+          )}
         </div>
         <div className="percentage">
-          <p>~{store.compatibility && store.compatibility.toFixed(0)}%~</p>
+          {!store.me ? (
+            <Skeleton type="text" />
+          ) : (
+            <p>~{store.compatibility && store.compatibility.toFixed(0)}%~</p>
+          )}
         </div>
-        <div
-          className="scroll-content"
-          onClick={() => scrollToPage("second-page")}
-        >
-          <p>
-            all it takes is a <span className="black">scroll up</span> to know
-            what{" "}
-            <span className="black">{`${
-              store.me && store.me.name.split(" ")[0]
-            }`}</span>{" "}
-            and{" "}
-            <span className="black">{`${
-              store.friend && store.friend.name.split(" ")[0]
-            }`}</span>
-            <br />
-            like to
-            <span className="black"> listen</span> to and imagine the{" "}
-            <span className="black">picturesque</span> of{" "}
-            {`${store.me && store.me.name.split(" ")[0]}`} x{" "}
-            {`${store.friend && store.friend.name.split(" ")[0]}`}.
-          </p>
-          <FiChevronDown />
-        </div>
+        {store.me && (
+          <div
+            className="scroll-content"
+            onClick={() => scrollToPage("second-page")}
+          >
+            <p>
+              all it takes is a <span className="black">scroll up</span> to know
+              what{" "}
+              <span className="black">{`${
+                store.me && store.me.name.split(" ")[0]
+              }`}</span>{" "}
+              and{" "}
+              <span className="black">{`${
+                store.friend && store.friend.name.split(" ")[0]
+              }`}</span>
+              <br />
+              like to
+              <span className="black"> listen</span> to and imagine the{" "}
+              <span className="black">picturesque</span> of{" "}
+              {`${store.me && store.me.name.split(" ")[0]}`} x{" "}
+              {`${store.friend && store.friend.name.split(" ")[0]}`}.
+            </p>
+            <FiChevronDown />
+          </div>
+        )}
       </div>
       <div className="second-page">
-        <div className="content-container">
-          <div className="title">Top Artist</div>
-          <div className="image-container">
-            <img
-              src={(store.topArtist && store.topArtist.image_url) || logo}
-              alt={store.topArtist && store.topArtist.name}
-            />
-            {store.topArtist && store.topArtist.topTrack.preview_url && (
-              <button
-                className="control"
-                onClick={() =>
-                  toggleAudioUrl(
-                    store.topArtist.topTrack.name,
-                    null,
-                    store.topArtist.image_url,
-                    store.topArtist.topTrack.preview_url
-                  )
-                }
-              >
-                {audioUrl === store.topArtist.topTrack.preview_url ? (
-                  <AiOutlinePause />
-                ) : (
-                  <AiFillCaretRight />
-                )}
-              </button>
-            )}
+        {!store.topArtist ? (
+          <div className="content-container">
+            <Skeleton type="text" />
+            <Skeleton type="text" />
+            <Skeleton type="text" />
           </div>
-          <div className="name">
-            {(store.topArtist && store.topArtist.name) || "No Artist"}
+        ) : (
+          <div className="content-container">
+            <div className="title">Top Artist</div>
+            <div className="image-container">
+              <img
+                src={(store.topArtist && store.topArtist.image_url) || logo}
+                alt={store.topArtist && store.topArtist.name}
+              />
+              {store.topArtist && store.topArtist.topTrack.preview_url && (
+                <button
+                  className="control"
+                  onClick={() =>
+                    toggleAudioUrl(
+                      store.topArtist.topTrack.name,
+                      null,
+                      store.topArtist.image_url,
+                      store.topArtist.topTrack.preview_url
+                    )
+                  }
+                >
+                  {audioUrl === store.topArtist.topTrack.preview_url ? (
+                    <AiOutlinePause />
+                  ) : (
+                    <AiFillCaretRight />
+                  )}
+                </button>
+              )}
+            </div>
+            <div className="name">
+              {(store.topArtist && store.topArtist.name) || "No Artist"}
+            </div>
           </div>
-        </div>
+        )}
         <div className="middle-content">
-          <p>
-            <span className="black">when it comes to these,</span>
-            <br />
-            {`${store.me && store.me.name.split(" ")[0]}`} and{" "}
-            {`${store.friend && store.friend.name.split(" ")[0]}`}{" "}
-            <span className="black">can’t hit the skip button!</span>
-          </p>
+          {!store.me ? (
+            <>
+              <Skeleton type="text" />
+              <Skeleton type="text" />
+            </>
+          ) : (
+            <p>
+              <span className="black">when it comes to these,</span>
+              <br />
+              {`${store.me && store.me.name.split(" ")[0]}`} and{" "}
+              {`${store.friend && store.friend.name.split(" ")[0]}`}{" "}
+              <span className="black">can’t hit the skip button!</span>
+            </p>
+          )}
         </div>
-        <div className="content-container">
-          <div className="title">Top Track</div>
-          <div className="image-container">
-            <img
-              src={(store.topTrack && store.topTrack.image_url) || logo}
-              alt={store.topTrack && store.topTrack.name}
-            />
-            {store.topTrack && store.topTrack.preview_url && (
-              <button
-                className="control"
-                onClick={() =>
-                  toggleAudioUrl(
-                    store.topTrack.name,
-                    store.topTrack.artist.map((item) => item.name).join(" "),
-                    store.topTrack.image_url,
-                    store.topTrack.preview_url
-                  )
-                }
-              >
-                {audioUrl === store.topTrack.preview_url ? (
-                  <AiOutlinePause />
-                ) : (
-                  <AiFillCaretRight />
-                )}
-              </button>
-            )}
+        {!store.topTrack ? (
+          <div className="content-container">
+            <Skeleton type="text" />
+            <Skeleton type="text" />
+            <Skeleton type="text" />
           </div>
-          <div className="name">
-            {(store.topTrack && store.topTrack.name) || "No Track"}
+        ) : (
+          <div className="content-container">
+            <div className="title">Top Track</div>
+            <div className="image-container">
+              <img
+                src={(store.topTrack && store.topTrack.image_url) || logo}
+                alt={store.topTrack && store.topTrack.name}
+              />
+              {store.topTrack && store.topTrack.preview_url && (
+                <button
+                  className="control"
+                  onClick={() =>
+                    toggleAudioUrl(
+                      store.topTrack.name,
+                      store.topTrack.artist.map((item) => item.name).join(" "),
+                      store.topTrack.image_url,
+                      store.topTrack.preview_url
+                    )
+                  }
+                >
+                  {audioUrl === store.topTrack.preview_url ? (
+                    <AiOutlinePause />
+                  ) : (
+                    <AiFillCaretRight />
+                  )}
+                </button>
+              )}
+            </div>
+            <div className="name">
+              {(store.topTrack && store.topTrack.name) || "No Track"}
+            </div>
           </div>
-        </div>
-        <div className="scroll-content">
-          <p>
-            wait<span className="black"> there's more, </span>
-            dig into more!
-          </p>
-          <FiChevronDown />
-        </div>
+        )}
+        {store.me && (
+          <div className="scroll-content">
+            <p>
+              wait<span className="black"> there's more, </span>
+              dig into more!
+            </p>
+            <FiChevronDown />
+          </div>
+        )}
       </div>
       {audioUrl && (
         <WebPlayer
