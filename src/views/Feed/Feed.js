9baace91eee20cb6d3c6dac6b5e07991a8ef7267
Sanket Mundada <mundadasanket2000@gmail.com>
2022-01-19 19:22:46 +0530

    Feed changes

--- src/views/Feed/Feed.js
+++ src/views/Feed/Feed.js
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import { FiSkipForward } from "react-icons/fi";
-import { FaRegHeart, FaHeart } from "react-icons/fa";
-import { axiosInstance } from "../../util/axiosConfig";
-import logo from "../../assets/images/logo-black.png";
 import jwtDecode from "jwt-decode";
+import React, { useEffect, useRef, useState } from "react";
 import { AiFillCaretRight, AiOutlinePause } from "react-icons/ai";
+import { GiSpeaker, GiSpeakerOff } from "react-icons/gi";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { FiSkipForward } from "react-icons/fi";
 import { useSetRecoilState } from "recoil";
-import { alertAtom } from "../../recoil/alertAtom";
-import { nFormatter, setMediaSession } from "../../util/functions";
+import logo from "../../assets/images/logo-black.png";
 import WebPlayer from "../../components/WebPlayer";
+import { alertAtom } from "../../recoil/alertAtom";
+import { axiosInstance } from "../../util/axiosConfig";
+import { nFormatter, setMediaSession } from "../../util/functions";
+import moment from "moment";
 
 const decodeJWT = () => {
   const access_token = localStorage.getItem("accessToken");
@@ -29,6 +30,7 @@ const Feed = () => {
     artists: "",
     imageUrl: null,
   });
+  const [display, setDisplay] = useState(false);
 
   const fetchPosts = () => {
     axiosInstance
@@ -65,6 +67,13 @@ const Feed = () => {
     if (index === posts.length - 3) {
       setPageId((prev) => prev + 1);
     }
+
+    // setCurrentSong({
+    //   audioUrl: posts[index]?.preview_url,
+    //   imageUrl: posts[index]?.image_url,
+    //   songName: posts[index]?.name,
+    //   artists: posts[index]?.artists.map((i) => i.name).join(", "),
+    // });
   }, [index]);
 
   useEffect(() => {
@@ -74,7 +83,7 @@ const Feed = () => {
         currentSong.artists,
         currentSong.imageUrl,
         null,
-        null
+        null,
       );
     }
   }, [currentSong.audioUrl]);
@@ -106,6 +115,11 @@ const Feed = () => {
   };
 
   const toggleAudioUrl = (item) => {
+    setDisplay(true);
+
+    setTimeout(() => {
+      setDisplay(false);
+    }, 800);
     if (!currentSong.audioUrl) {
       setCurrentSong({
         audioUrl: item.preview_url,
@@ -134,7 +148,6 @@ const Feed = () => {
                   key={item.feed_id}
                   className="ind-post"
                   onClick={() => toggleAudioUrl(item)}
-                  onDoubleClick={() => likePost(item.feed_id)}
                 >
                   <div className="image-container">
                     <img src={item.image_url} alt="Dummy" />
@@ -148,7 +161,9 @@ const Feed = () => {
                         </div>
                         <div className="user-meta">
                           <p className="name">{item.display_name}</p>
-                          <p className="time">4d ago</p>
+                          <p className="time">
+                            {moment(item.created_at).fromNow()}
+                          </p>
                         </div>
                       </div>
                       <div className="song-info">
@@ -162,7 +177,12 @@ const Feed = () => {
                           {userId && item.likes.includes(userId) ? (
                             <FaHeart />
                           ) : (
-                            <FaRegHeart />
+                            <FaRegHeart
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                likePost(item.feed_id);
+                              }}
+                            />
                           )}
                           <span>{nFormatter(item.likes.length)}</span>
                         </div>
@@ -170,11 +190,16 @@ const Feed = () => {
                     </div>
                   </div>
                   <div className="overlay">
-                    <div className="controls">
+                    <div
+                      className="controls"
+                      style={{ display: display ? "" : "none" }}
+                    >
                       {currentSong.audioUrl === item.preview_url ? (
-                        <AiFillCaretRight />
+                        // <AiFillCaretRight />
+                        <GiSpeaker />
                       ) : (
-                        <AiOutlinePause />
+                        // <AiOutlinePause />
+                        <GiSpeakerOff />
                       )}
                     </div>
                   </div>
