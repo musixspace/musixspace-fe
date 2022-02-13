98321d89810f26d5908a5d8363975df0bae7eaf1
Sanket Mundada <mundadasanket2000@gmail.com>
2022-02-13 09:57:50 +0530

    basic ui stuff added

--- src/views/Chat/ChatWindow.js
+++ src/views/Chat/ChatWindow.js
@@ -4,6 +4,10 @@ import { useSocket } from "../../context/socketContext";
 import { userState } from "../../recoil/userAtom";
 import { axiosInstance } from "../../util/axiosConfig";
 
+import { FaSmile } from "react-icons/fa";
+import { BsMusicNoteList } from "react-icons/bs";
+import { GrSend } from "react-icons/gr";
+
 const ChatWindow = ({ selectedChat }) => {
   const [messages, setMessages] = useState([]);
   const [pageId, setPageId] = useState(0);
@@ -12,6 +16,8 @@ const ChatWindow = ({ selectedChat }) => {
   const user = useRecoilValue(userState);
   console.log(user.userId);
   const { userId } = user;
+
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
   const to_id = participants.find((participant) => participant !== userId); // need to extract userId here
 
@@ -30,7 +36,7 @@ const ChatWindow = ({ selectedChat }) => {
     (async () => {
       if (chat_id) {
         const resp = await axiosInstance.get(
-          `/chat/messages/${chat_id}/${pageId}`
+          `/chat/messages/${chat_id}/${pageId}`,
         );
         console.log(resp);
         setMessages(resp.data);
@@ -47,19 +53,54 @@ const ChatWindow = ({ selectedChat }) => {
     }
   };
 
+  const onEmojiClick = () => {};
   return (
-    <div>
-      {messages.map((msg) => (
+    <div className="chattyWrapper">
+      <div className="chatHeader">
+        <div className="imageContainer">
+          <img src={selectedChat.otherUser.image_url} className="userImg" />
+        </div>
+        <p>{selectedChat.otherUser.display_name}</p>
+      </div>
+      {/* {messages.map((msg) => (
         <div key={msg.message_id}>{JSON.stringify(msg.content)}</div>
-      ))}
-      <input
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-        placeholder="Type your message"
-      />
-      <button onClick={sendMessage}>Send</button>
+      ))} */}
+      <div className="chatsMain"></div>
+      <div className="inputBar">
+        <div className="emojiContainer">
+          <button
+            onClick={() => {
+              setShowEmojiPicker((prev) => !prev);
+            }}
+            className="emojiBtn"
+          >
+            <FaSmile />
+          </button>
+        </div>
+        <div className="audioContainer">
+          <button
+            onClick={() => {
+              setShowEmojiPicker((prev) => !prev);
+            }}
+            className="audioBtn"
+          >
+            <BsMusicNoteList />
+          </button>
+        </div>
+        <div className="inputContainer">
+          <input
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            placeholder="Type your message"
+            className="inputMsg"
+          />
+        </div>
+        <button className="sendMsgBtn" onClick={sendMessage}>
+          <GrSend />
+        </button>
+      </div>
     </div>
   );
 };
