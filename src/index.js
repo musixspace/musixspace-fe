45de1c0dcb22123181f951cd083d5f98c4547674
Sanket Mundada <mundadasanket2000@gmail.com>
2022-02-25 12:43:59 +0530

    seo sorted as of now

--- src/index.js
+++ src/index.js
@@ -5,38 +5,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./assets/scss/index.scss";
 import { RecoilRoot } from "recoil";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(
-    <React.StrictMode>
-      <Router>
-        <RecoilRoot>
-          <App />
-        </RecoilRoot>
-      </Router>
-    </React.StrictMode>,
-    rootElement,
-  );
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Router>
-        <RecoilRoot>
-          <App />
-        </RecoilRoot>
-      </Router>
-    </React.StrictMode>,
-    rootElement,
-  );
-}
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Router>
-//       <RecoilRoot>
-//         <App />
-//       </RecoilRoot>
-//     </Router>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
