112e516cd08b49c04cc232370f3e682375fe2f65
Atharva Jangada <atharva@Atharvas-MacBook-Pro.local>
2022-02-25 22:12:59 +0530

    added dynamic sitemap generation script

---
+++ router.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+export default (
+    <Switch>
+        <Route path='/' />
+        <Route path='/about' />
+        <Route path='/readytorock' />
+        <Route path='/rolling' />
+        <Route path='/insights' />
+        <Route path='/insights/toptracks' />
+        <Route path='/insights/topartists' />
+        <Route path='/insights/surprise' />
+        <Route path='/insights/mood' />
+        <Route path='/feed' />
+        <Route path='/feed/:id' />
+        <Route path='/discover' />
+        <Route path='/match/:matchHandle' />
+        <Route path='/chat' />
+        <Route path='/:handle' />
+    </Switch>
+); 
---
+++ sitemap-generator.js
@@ -0,0 +1,120 @@
+const axios = require("axios");
+
+require("babel-register")({
+  presets: ["es2015", "react"],
+});
+
+const router = require("./router").default;
+const Sitemap = require("react-router-sitemap").default;
+
+const generateSitemap = async () => {
+  const users = await axios.get(
+    "http://35.154.242.251/api/sitemap/getAllUsers"
+  );
+  const feeds = await axios.get(
+    "http://35.154.242.251/api/sitemap/getAllFeeds"
+  );
+
+  const handleMap = [],
+    feedMap = [],
+    userIdMap = [];
+
+  users.data.forEach((item) => {
+    userIdMap.push({ matchHandle: item.username });
+    handleMap.push({ handle: item.username });
+  });
+
+  feeds.data.forEach((item) => {
+    feedMap.push({ id: item.feed_id });
+  });
+
+  const paramsConfig = {
+    "/feed/:id": feedMap,
+    "/match/:matchHandle": userIdMap,
+    "/:handle": handleMap,
+  };
+
+  return new Sitemap(router)
+    .applyParams(paramsConfig)
+    .build("http://35.154.242.251")
+    .save("./public/sitemap.xml");
+};
+
+generateSitemap();
+
+// require('babel-register');
+// const shows= require('./shows.json');
+// const genres= require('./genres.json');
+// const countries= require('./countries.json');
+
+// //Import our routes
+// const router = require("./router").default;
+// const Sitemap = require("react-router-sitemap").default;
+
+// function generateSitemap() {
+//   let idMap = [];
+
+//   for(let i = 0; i < shows.length; i++) {
+//     let obj = shows[i];
+//     idMap.push({ showId: obj.podcast_id, showSlug:obj.slug});
+//   }
+
+//   let idMap2 = [];
+
+//   for(let i = 0; i < countries.length; i++) {
+//     for(let j = 0; j < genres.length; j++) {
+
+//       let obj = countries[i].name;
+//       let obj2= genres[j].name;
+
+//       let Aobj=obj;
+//       let Bobj='';
+//       for(let ii=0;ii<Aobj.length;ii++)
+//       {
+//         if(Aobj[ii]==' ')
+//         {
+//           Bobj=Bobj+'%20';
+//         }
+//         else
+//         {
+//           Bobj=Bobj+Aobj[ii];
+//         }
+//       }
+//       // Bobj=Bobj.toLowerCase();
+
+//       let Aobj2=obj2;
+//       let Bobj2='';
+//       for(let ii=0;ii<Aobj2.length;ii++)
+//       {
+//         if(Aobj2[ii]==' ')
+//         {
+//           Bobj2=Bobj2+'%20';
+//         }
+//         else
+//         {
+//           Bobj2=Bobj2+Aobj2[ii];
+//         }
+//       }
+//       // Bobj2=Bobj2.toLowerCase();
+
+//       idMap2.push({ showCountry: Bobj, showGenre:Bobj2});
+//     }
+//   }
+
+//   const paramsConfig = {
+//     "/podcast/:showId/:showSlug": idMap,
+//     "/rankings/:showCountry/:showGenre":idMap2
+//   };
+
+//   return (
+//     new Sitemap(router).applyParams(paramsConfig)
+//     //use accordingly
+//     // .build("http://app0.pikkalfm.com")
+//     // .build("http://localhost:3000")
+//     .build("https://www.podminer.com")
+//     // .save("public/sitemap.xml")
+//     .save("build/sitemap.xml")
+//   );
+// }
+
+// generateSitemap();
